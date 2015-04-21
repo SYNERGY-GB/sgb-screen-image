a0e05ef4ee83e688c1c5755307a2a258e69c4121
Juan Garcia <juan.garcia@synergy-gb.com>
2015-04-21 15:20:54 -0500

    Finalizado ajuste inicial de la pantalla y agregado el archivo bower.json

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('build', function(){
     return b.bundle();
   });
 
-  return gulp.src('src/sgb-screen-schedule.js')
+  return gulp.src('src/sgb-screen-image.js')
     .pipe(browserified)
     .pipe(gulp.dest('./dist/'));
 });
---
+++ src/sgb-screen-image.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angula.module('sgb-screen-image', ['megazord'])
+  .controller('sgb-screen-image-controller', ['$stateParams', '$scope', function($stateParams, $scope){
+    $scope.image = $stateParams.data;
+  }]);
