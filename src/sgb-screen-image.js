f85b4028a48b2aea8d7010166768d77586e0f2dc
Juan Garcia <juan.garcia@synergy-gb.com>
2015-04-23 16:49:27 -0500

    Regenerando Dist

--- src/sgb-screen-image.js
+++ src/sgb-screen-image.js
@@ -3,4 +3,7 @@
 angular.module('sgb-screen-image', ['megazord'])
   .controller('sgb-screen-image-controller', ['$stateParams', '$scope', function($stateParams, $scope){
     $scope.image = $stateParams.data;
+
+    
+
   }]);
