1e2a9e4df882548f0e044769444bec583b8d644a
Saul Hernandez <sauldhernandez@gmail.com>
2015-04-26 13:49:52 -0430

    Screen title can now be configured

--- src/sgb-screen-image.js
+++ src/sgb-screen-image.js
@@ -1,9 +1,10 @@
 'use strict';
 
 angular.module('sgb-screen-image', ['megazord'])
-  .controller('sgb-screen-image-controller', ['$stateParams', '$scope', function($stateParams, $scope){
-    $scope.image = $stateParams.data;
+  .controller('sgb-screen-image-controller', ['$stateParams', '_screenParams', '$scope', function($stateParams, _screenParams, $scope){
+      $scope.title = _screenParams.title || 'image_view_title';
+      $scope.image = $stateParams.data;
 
-    $scope.init = function(){};
+      $scope.init = function(){};
 
   }]);
