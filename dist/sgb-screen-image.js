1170a1963497a2df5fc9503ca4762e7665f4c787
Saul Hernandez <sauldhernandez@gmail.com>
2015-05-13 22:28:29 -0430

    Now properly calls initialize

--- dist/sgb-screen-image.js
+++ dist/sgb-screen-image.js
@@ -2,11 +2,14 @@
 'use strict';
 
 angular.module('sgb-screen-image', ['megazord'])
-  .controller('sgb-screen-image-controller', ['$stateParams', '_screenParams', '$scope', function($stateParams, _screenParams, $scope){
-      $scope.title = _screenParams.title || 'image_view_title';
-      $scope.image = $stateParams.data;
+    .controller('sgb-screen-image-controller', ['$stateParams', '_screen', '_screenParams', '$scope', function ($stateParams, _screen, _screenParams, $scope) {
 
-      $scope.init = function(){};
+        _screen.initialize($scope, _screenParams);
 
-  }]);
+        $scope.image = $stateParams.data;
+
+        $scope.init = function () {
+        };
+
+    }]);
 },{}]},{},[1]);
